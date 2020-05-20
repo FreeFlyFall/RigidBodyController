5db67c5680c7af1c1d13f1d2eaae3aaed3e6f533
FreeFlyFall <ryanjvanslyke@gmail.com>
2020-05-19 20:53:02 -0400

    Basic directional input FINALLY behaves similar to a kinematic body after further restricting velocities based on look direction. Fixed the issue of being able to move against the axis being looked towards if the current and move vectors had opposite signs. This issue showed itself when looking slightly off of a cardinal axis, and moving in a direction before tapping the direction which would set the move vector across the axis. Momentum would be added in that direction without being limited to the normalized move vector.

---
+++ Scenes/cuberb.gd
@@ -0,0 +1,12 @@
+extends RigidBody
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var once = false
+func _integrate_forces(state):	
+	if (once == false):
+		once = true
+		var direction = Vector3(0,0,1)
+		state.apply_central_impulse(direction*1000)
--- move.gd
+++ move.gd
@@ -1,51 +1,86 @@
 extends Spatial
 
-var speed = 15
-var gravity = 9.8
-var jump = 9
-var mouse_sensitivity = 0.05
+var speed := 10
+var gravity := 9.8
+var jump := 1
+var mouse_sensitivity := 0.05
+var acceleration := 1
+var rough_friction := 4
+var no_friction := 1
 
-var direction = Vector3()
-var mouse_captured
+var direction: Vector3 = Vector3()
+var mouse_captured: bool
 
 onready var head = $Head
 onready var rot = $Head/Rot
 
-### Player Look
+var player_physics_material = load("res://Physics/player.tres")
+
+# Player Look
 func _input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 		rot.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 		rot.rotation.x = clamp(rot.rotation.x, deg2rad(-90), deg2rad(90))
 
-### Mouse capture/free
+# Capture mouse on load
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
-	mouse_captured = 1
+	mouse_captured = true
+
+# Return true if any of the movement keys were just pressed
+func is_any_movement_just_pressed():
+	return (Input.is_action_just_pressed("move_forward") or
+	 Input.is_action_just_pressed("move_backward") or
+	 Input.is_action_just_pressed("move_left") or
+	 Input.is_action_just_pressed("move_right"))
+
+# Returns true if all movement keys are released after at least one was pressed
+func is_all_movement_just_released():
+	if (Input.is_action_just_released("move_forward") or
+	 Input.is_action_just_released("move_backward") or
+	 Input.is_action_just_released("move_left") or
+	 Input.is_action_just_released("move_right")):
+		return (!Input.is_action_pressed("move_forward") and
+		 !Input.is_action_pressed("move_backward") and
+		 !Input.is_action_pressed("move_right") and
+		 !Input.is_action_pressed("move_left"))
+	
 func _process(_delta):
+	# Capture/release mouse on 'Esc' press
 	if Input.is_action_just_pressed("ui_cancel"):
-		if (mouse_captured == 1):
+		if (mouse_captured == true):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			mouse_captured = 0
+			mouse_captured = false
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			mouse_captured = 1
+			mouse_captured = false
+			
+	# Any input pressed -> remove rigidbody friction to prevent sticking (mimics move and slide)
+	if (is_any_movement_just_pressed()):
+		player_physics_material.rough = false
+		player_physics_material.friction = no_friction
+	# All input released -> change rigidbody physics material to high friction
+	elif (is_all_movement_just_released()):
+		player_physics_material.rough = true
+		player_physics_material.friction = rough_friction
+			
+#DevNotes:
+	# If jumped and lets go of space, apply slight downward force
+	# If forced above max velocity(by impulse or collision), don't do velocity limiting during input except
+	 #in the axes on which the velocity is over the max speed
+	# FSM for Rigidbody controller?:
+		# Air(If not grounded) - (add small force in direction held)
+		# Idle(no input) - (Act against any current vel), or run timer and dec vel with it until stopped
+		# Accel(input) - (Force towards direction),
+		# AtSpeed(grounded, witht input, at speed limit) - (SetVelocity state if over velocity limit and direction held)
+	# remove friction in the air as well. On becoming grounded, scale it up over a fraction of a second to allow a small slide.
+	 # I don't think I can work around that with the given physics
 
-# FSM for Rigidbody controller?
-	# Air(If not grounded) - (add small force in direction held if cannot subtract velocity directly)
-	# Idle(no input) - (Act against any current vel), or run timer and dec vel with it until stopped
-	# Accel(input) - (Force towards direction),
-	# AtSpeed(grounded, witht input, at speed limit) - (SetVelocity state if over velocity limit and direction held)
 
-var once = false
 func _integrate_forces(state):	
 	# Before continuing, check an is_grounded raycast
 		# if not grounded, use air-controls
-		# prevent popping up when moving on slopes and jumping up to edges because of capsule collider
-		# prevent sliding when becoming grounded
-	
-	# Consider using friction and creating a move_and_slide function for the
-	# Change body material when not input to be one with high friction
 	
 	# Handle jump
 	if (Input.is_action_just_pressed("jump")):
@@ -53,76 +88,55 @@ func _integrate_forces(state):
 	
 	var move = Vector3()
 	var current = state.get_linear_velocity()
-	
-	if (once == false):
-		once = true
-		direction = -head.transform.basis.z
-		state.apply_central_impulse(direction*20)
-	
-	## Velocity limits need to be removed for impulses & collisions during input
-	# Handle diagonal movements # Stop will be handled by single key press logic
-	if (Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_right")): 
-		move = (-head.transform.basis.z + head.transform.basis.x).normalized() * speed
-		move.y = current.y
-		state.set_linear_velocity(move)
-	elif (Input.is_action_pressed("move_backward") and Input.is_action_pressed("move_right")): 
-		move = (head.transform.basis.z + head.transform.basis.x).normalized() * speed
-		move.y = current.y
-		state.set_linear_velocity(move)
-	elif (Input.is_action_pressed("move_backward") and Input.is_action_pressed("move_left")): 
-		move = (head.transform.basis.z + -head.transform.basis.x).normalized() * speed
-		move.y = current.y
-		state.set_linear_velocity(move)
-	elif (Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_left")): 
-		move = (-head.transform.basis.z + -head.transform.basis.x).normalized() * speed
-		move.y = current.y
-		state.set_linear_velocity(move)
 
-	# Handle z-axis movement
+	# Handle diagonal inputs
+	if (Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_right")): 
+		# Add the closest 2 cardinal x/z vectors and normalize the result
+		move = (-head.transform.basis.z + head.transform.basis.x).normalized()
+	elif (Input.is_action_pressed("move_backward") and Input.is_action_pressed("move_right")): 
+		move = (head.transform.basis.z + head.transform.basis.x).normalized()
+	elif (Input.is_action_pressed("move_backward") and Input.is_action_pressed("move_left")): 
+		move = (head.transform.basis.z + -head.transform.basis.x).normalized()
+	elif (Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_left")): 
+		move = (-head.transform.basis.z + -head.transform.basis.x).normalized()
+		
+	# Handle z-axis inputs
 	elif Input.is_action_pressed("move_forward"):
 		# Normalized movement vector based on forward look direction (-z), multiplied by speed
-		move = -head.transform.basis.z * speed
-
-		current = state.get_linear_velocity()
-		var limit = current # The current velocity to be limited
-		# set x-axis speed limit for normalized movement based on look direction
-		if (move.x > 0 and current.x > move.x) || (move.x < 0 and current.x < move.x):
-			limit.x = move.x
-		# set z-axis speed limit for normalized movement based on look direction
-		if (move.z > 0 and current.z > move.z) || (move.z < 0 and current.z < move.z):
-			limit.z = move.z
-		# If an axis was over the limit, it will be set to the limit in the limit variable
-		state.set_linear_velocity(limit)
-		# Add the intended force
-		state.add_central_force(move)
-
-### Refactor 
-	elif (Input.is_action_just_released("move_forward")):
-		move = Vector3()
-		move.y = current.y
-		state.set_linear_velocity(move)
+		move = -head.transform.basis.z
 	elif Input.is_action_pressed("move_backward"):
-		move = head.transform.basis.z * speed
+		move = head.transform.basis.z
 		
-		move.y = current.y
-		state.set_linear_velocity(move)
-		print(state.get_linear_velocity())
-	elif Input.is_action_just_released("move_backward"):
-		move.y = current.y
-		state.set_linear_velocity(move)
-
-	# Handle x-axis movement
+	# Handle x-axis inputs
 	elif Input.is_action_pressed("move_right"):
-		move = head.transform.basis.x *  speed
-		move.y = current.y
-		state.set_linear_velocity(move)
-	elif Input.is_action_just_released("move_right"):
-		move.y = current.y
-		state.set_linear_velocity(move)
+		# Normalized movement vector based on sideways direction (x), multiplied by speed
+		move = head.transform.basis.x
 	elif Input.is_action_pressed("move_left"):
-		move = -head.transform.basis.x * speed
-		move.y = current.y
-		state.set_linear_velocity(move)
-	elif Input.is_action_just_released("move_left"):
-		move.y = current.y
-		state.set_linear_velocity(move)
+		move = -head.transform.basis.x
+
+	move *= speed # Multiply the normalized move vector by speed
+	current = state.get_linear_velocity()
+	var limit = current # The current velocity to be limited
+
+	### set x and z axes speed limit for normalized movement based on move direction ###
+	#	X-AXIS
+	# If the move vector is in the direction for the respective axis the player is moving,
+	# and the current velocity is greater than the normalized limit for that direction,
+	if ((move.x > 0 and current.x > move.x) or
+	 (move.x < 0 and current.x < move.x) or
+	 # or if the current velocity is in the opposite direction of the move vector for this axis,
+	 (current.x < 0 and move.x > 0) or
+	 (current.x > 0 and move.x < 0)):
+	 # set the velocity limit according to the look direction
+		limit.x = move.x
+	#   Z-AXIS 
+	if ((move.z > 0 and current.z > move.z) or 
+	 (move.z < 0 and current.z < move.z) or
+	 (current.z < 0 and move.z > 0) or
+	 (current.z > 0 and move.x < 0)):
+		limit.z = move.z
+	
+	# If an axis was over the limit, it will be set to the limit in the limit variable
+	state.set_linear_velocity(limit)
+	# Add the intended force
+	state.add_central_force(move * acceleration)
